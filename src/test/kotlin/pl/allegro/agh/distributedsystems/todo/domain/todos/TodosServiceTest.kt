2b1b9875cb116013dcb51e659df1d75af376d420
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-10-19 16:32:49 +0200

    Create DummyUserRepository test double

--- src/test/kotlin/pl/allegro/agh/distributedsystems/todo/domain/todos/TodosServiceTest.kt
+++ src/test/kotlin/pl/allegro/agh/distributedsystems/todo/domain/todos/TodosServiceTest.kt
@@ -1,28 +1,18 @@
 package pl.allegro.agh.distributedsystems.todo.domain.todos
 
-import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.junit5.MockKExtension
-import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import pl.allegro.agh.distributedsystems.todo.domain.users.User
 import pl.allegro.agh.distributedsystems.todo.domain.users.UserRepository
 import pl.allegro.agh.distributedsystems.todo.infrastructure.todos.InMemoryTodosRepository
-import strikt.api.expectThat
 import strikt.api.expectThrows
-import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
 import strikt.assertions.message
 
-@ExtendWith(MockKExtension::class)
-class TodosServiceTest(
-    @RelaxedMockK private val userRepository: UserRepository,
-) {
+class TodosServiceTest {
     private val repository = InMemoryTodosRepository()
+    private val userRepository = DummyUserRepository()
     private val service = TodosService(repository, userRepository)
 
     @AfterEach
@@ -30,53 +20,54 @@ class TodosServiceTest(
         repository.clear()
     }
 
-    @Nested
-    inner class `active user` {
-        private val activeUser = "user"
-
-        @BeforeEach
-        fun `set active user`() {
-            every { userRepository.findByName(any()) } returns User(
-                username = activeUser,
-                status = User.Status.ACTIVE,
-            )
-        }
-
-        @Test
-        fun `generate unique ids on save`() {
-            val uniqueIds = (1..100)
-                .map { service.save(activeUser, "todo name $it") }
-                .map { it.id }
-                .toSet()
-
-            expectThat(uniqueIds).hasSize(100)
-        }
-    }
+//    @Nested
+//    inner class `active user` {
+//        private val activeUser = "user"
+//
+//        @BeforeEach
+//        fun `set active user`() {
+//            every { userRepository.findByName(any()) } returns User(
+//                username = activeUser,
+//                status = User.Status.ACTIVE,
+//            )
+//        }
+//
+//        @Test
+//        fun `generate unique ids on save`() {
+//            val uniqueIds = (1..100)
+//                .map { service.save(activeUser, "todo name $it") }
+//                .map { it.id }
+//                .toSet()
+//
+//            expectThat(uniqueIds).hasSize(100)
+//        }
+//    }
 
     @Nested
     inner class `user blocking` {
 
         @Test
         fun `reject saves by missing user`() {
-            every { userRepository.findByName(any()) } returns null
-
             expectThrows<TodosService.CannotSaveException> {
                 service.save("user", "todo name")
             }.message.isEqualTo("User is not active")
-            verify(exactly = 1) { userRepository.findByName("user") }
         }
 
-        @Test
-        fun `reject saves by blocked user`() {
-            every { userRepository.findByName(any()) } returns User(
-                username = "user",
-                status = User.Status.BLOCKED,
-            )
-
-            expectThrows<TodosService.CannotSaveException> {
-                service.save("user", "todo name")
-            }.message.isEqualTo("User is not active")
-            verify(exactly = 1) { userRepository.findByName("user") }
-        }
+//        @Test
+//        fun `reject saves by blocked user`() {
+//            every { userRepository.findByName(any()) } returns User(
+//                username = "user",
+//                status = User.Status.BLOCKED,
+//            )
+//
+//            expectThrows<TodosService.CannotSaveException> {
+//                service.save("user", "todo name")
+//            }.message.isEqualTo("User is not active")
+//            verify(exactly = 1) { userRepository.findByName("user") }
+//        }
     }
+}
+
+class DummyUserRepository : UserRepository {
+    override fun findByName(username: String): User? = null
 }
