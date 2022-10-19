19c5aa2b0a0bb2f4749be9b36c96b88a23222c25
Adam Szadkowski <adam.szadkowski@allegro.pl>
2022-10-19 16:50:31 +0200

    Create MockUserRepository test double

--- src/test/kotlin/pl/allegro/agh/distributedsystems/todo/domain/todos/TodosServiceTest.kt
+++ src/test/kotlin/pl/allegro/agh/distributedsystems/todo/domain/todos/TodosServiceTest.kt
@@ -7,16 +7,14 @@ import org.junit.jupiter.api.Test
 import pl.allegro.agh.distributedsystems.todo.domain.users.User
 import pl.allegro.agh.distributedsystems.todo.domain.users.UserRepository
 import pl.allegro.agh.distributedsystems.todo.infrastructure.todos.InMemoryTodosRepository
+import strikt.api.expectCatching
 import strikt.api.expectThat
 import strikt.api.expectThrows
-import strikt.assertions.hasEntry
-import strikt.assertions.hasSize
-import strikt.assertions.isEqualTo
-import strikt.assertions.message
+import strikt.assertions.*
 
 class TodosServiceTest {
     private val repository = InMemoryTodosRepository()
-    private val userRepository = SpyUserRepository()
+    private val userRepository = MockUserRepository()
     private val service = TodosService(repository, userRepository)
 
     @AfterEach
@@ -30,7 +28,7 @@ class TodosServiceTest {
 
         @BeforeEach
         fun `set active user`() {
-            userRepository.users[activeUser] = User(
+            userRepository.users.computeIfAbsent(activeUser) { mutableListOf() } += User(
                 username = activeUser,
                 status = User.Status.ACTIVE,
             )
@@ -59,7 +57,7 @@ class TodosServiceTest {
 
         @Test
         fun `reject saves by blocked user`() {
-            userRepository.users["user"] = User(
+            userRepository.users.computeIfAbsent("user") { mutableListOf() } += User(
                 username = "user",
                 status = User.Status.BLOCKED,
             )
@@ -69,15 +67,33 @@ class TodosServiceTest {
             }.message.isEqualTo("User is not active")
             expectThat(userRepository.calls).hasEntry("user", 1)
         }
+
+        @Test
+        fun `check user status twice`() {
+            val responses = userRepository.users.computeIfAbsent("user") { mutableListOf() }
+            responses += User(
+                username = "user",
+                status = User.Status.BLOCKED,
+            )
+            responses += User(
+                username = "user",
+                status = User.Status.ACTIVE,
+            )
+
+            expectCatching { service.save("user", "todo name") }.isFailure()
+            expectCatching { service.save("user", "todo name") }.isSuccess()
+        }
     }
 }
 
-class SpyUserRepository : UserRepository {
-    val users: MutableMap<String, User> = mutableMapOf()
+class MockUserRepository : UserRepository {
+    val users: MutableMap<String, MutableList<User>> = mutableMapOf()
     val calls: MutableMap<String, Int> = mutableMapOf()
 
     override fun findByName(username: String): User? {
         calls.compute(username) { _, prev -> (prev ?: 0) + 1 }
+        val responseIndex = minOf(calls.getValue(username), users[username]?.size ?: 0) - 1
         return users[username]
+            ?.get(responseIndex)
     }
 }
